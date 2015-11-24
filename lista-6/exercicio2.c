725de87750b21e71151e5fe722a6d7c91ecf63e3
Lucas Nery <nerylyc@gmail.com>
2015-11-24 16:20:51 +0000

    Adicionando exercicios da lista 6

---
+++ lista-6/exercicio2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void jogo_da_senha(){
+    int senhaInvalida = 1;
+    int tentativas = 5;
+    int tentativa_atual = 1;
+    int senha;
+    int senha_advinha;
+    int jogador2_venceu = 0;
+    
+    while(senhaInvalida){
+        printf("Jogador 1 - Digite a senha (entre 1 a 100): ");
+        scanf("%d", &senha);
+        
+        if(senha >= 1 && senha <= 100)
+            senhaInvalida = 0;
+    }
+    
+    while(tentativa_atual <= tentativas){
+        printf("\nJogador 2 - Tente adivinhar a senha (tentativa %d de %d): ", tentativa_atual, tentativas);
+        scanf("%d", &senha_advinha);
+        
+        if(senha_advinha == senha){
+            printf("Parabens! Voce acertou!");
+            break;
+        } else if(senha_advinha == (senha + 1) || senha_advinha == (senha - 1)){
+            printf("TA QUENTE!");
+        } else {
+            printf("Passou longe...");
+        }
+        
+        if(tentativa_atual == tentativas){
+            printf("\nVoce perdeu! :(");
+        }
+        
+        tentativa_atual++;
+    }
+    
+    printf("\n");
+}
+
+int main(){
+    jogo_da_senha();
+    return 0;
+}
